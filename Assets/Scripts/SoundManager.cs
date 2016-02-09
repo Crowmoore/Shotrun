fb8715e658212a280c6909fe7c9be0cfcbd32be3
Joel Kortelainen <jokke.kortelainen@gmail.com>
2016-02-09 17:15:47 +0200

    Added Soundmanager and global Volume managing scripts

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	private AudioSource music;
+	private bool isCreated;
+
+	void Awake() {
+		if(!isCreated) {
+			DontDestroyOnLoad(gameObject);
+		}
+		else {
+			Destroy(gameObject);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		music = GetComponent<AudioSource>();
+		music.volume = Volume.musicVolume;
+	}
+}
---
+++ Assets/Scripts/Volume.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Volume : MonoBehaviour {
+
+	public static float sfxVolume;
+	public static float musicVolume;
+
+	// Use this for initialization
+	void Start () {
+		SetSFXVolume();
+		SetMusicVolume();
+	}
+	private void SetSFXVolume() {
+		sfxVolume = GameObject.Find("SoundSlider").GetComponent<Slider>().value;
+	}
+	private void SetMusicVolume() {
+		musicVolume = GameObject.Find("MusicSlider").GetComponent<Slider>().value;
+	}
+	public void OnSoundValueChange(float newValue) {
+		sfxVolume = newValue;
+	}
+	public void OnMusicValueChange(float newValue) {
+		musicVolume = newValue;
+		GameObject.Find("Music").GetComponent<AudioSource>().volume = musicVolume;
+	}
+}
